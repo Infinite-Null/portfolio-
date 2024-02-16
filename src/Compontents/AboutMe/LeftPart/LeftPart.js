f70da80939aa217843599e54cb824d9fa4d2cdc9
Infinite-Null <ankit.kum.sha9933@gmail.com>
2024-02-16 22:06:27 +0530

    Improved Image

--- src/Compontents/AboutMe/LeftPart/LeftPart.js
+++ src/Compontents/AboutMe/LeftPart/LeftPart.js
@@ -5,7 +5,7 @@ import {motion} from 'framer-motion'
 export default function LeftPart() {
   return (
     <div className='AboutMeLeftPartMainContainer'>
-        <TextAnimation phrase={'An enthusiastic student who likes coding lengthy scripts to creatively solve strange problems. When I get free time, I enjoy spending that time designing and creating website. I am really passionate about tech and learning new programming languages and I love to develop new things like some program and websiteFor now, I am practicing web development while also learning flutter to develop mobile applications, with a desire to be a skilled software developer.'}/>
+        <TextAnimation phrase={'An enthusiastic student who likes coding lengthy scripts to creatively solve strange problems. When I get free time, I enjoy spending that time designing and creating website. I am really passionate about tech and learning new programming languages and I love to develop new things like some program and website For now, I am practicing web development while also learning flutter to develop mobile applications, with a desire to be a skilled software developer.'}/>
        <motion.button initial={{
            y:60,
        }} whileInView={{
--- src/Parts/Home/AboutMe.js
+++ src/Parts/Home/AboutMe.js
@@ -2,7 +2,10 @@ import React from 'react'
 import "./AboutMe.css"
 import LeftPart from '../../Compontents/AboutMe/LeftPart/LeftPart'
 import Heading from '../../Compontents/Global/Heading/Heading'
-import Image from "../../Images/WhatsApp Image 2024-02-15 at 3.35.03 PM.jpeg"
+import Image from "../../Images/coding-3d-render-icon-illustration-png.png"
+import Image1 from "../../Images/534621.png"
+import Image2 from "../../Images/20240214_163943_yellow.PORTRAIT.jpg"
+import {motion} from "framer-motion";
 
 export default function AboutMe() {
   return (
@@ -10,7 +13,25 @@ export default function AboutMe() {
           <Heading/>
           <div className='AboutMeLeftAndImage'>
               <LeftPart/>
-              <img src={Image} alt={''}/>
+              <div className={"AboutMeImageContainer"}>
+                  <img className={"AboutMeMainImage"} src={Image2} alt={''}/>
+                  <motion.img initial={{
+                y:100,
+                opacity:0
+            }} whileInView={{
+                y:0,
+                opacity:1,
+                transition:{duration:1,ease: [0.9, 0, 0.24, 1]}
+            }} className={"AboutMeImage1"} src={Image} alt={''}/>
+                  <motion.img initial={{
+                y:100,
+                opacity:0
+            }} whileInView={{
+                y:0,
+                opacity:1,
+                transition:{duration:0.5,ease: [0.9, 0, 0.24, 1]}
+            }} className={"AboutMeImage2"} src={Image1} alt={''}/>
+              </div>
           </div>
           <div className='BottomAboutME' />
       </div>
