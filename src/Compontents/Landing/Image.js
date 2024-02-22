b6f27b75cde3d897fd7310f5d8776a04d5ca4684
Infinite-Null <ankit.kum.sha9933@gmail.com>
2024-02-23 00:58:31 +0530

    Made responsive

--- src/Compontents/Landing/Image.js
+++ src/Compontents/Landing/Image.js
@@ -1,21 +1,10 @@
 import React from 'react'
 import img from "../../Images/20240215_004439.png"
+import "./Image.css"
 export default function Image() {
   return (
-       <div style={{
-          position:'absolute',
-          bottom:0,
-          width:'100vw',
-          display:'flex',
-          alignItems:'center',
-          justifyContent:'center',
-          zIndex:'1'
-        }}>
-          <img src={img} alt='' style={{
-            height:"56vw",
-            width:"56vw",
-            objectFit:"cover",
-          }}/> 
+       <div className={"MainLandingImage"} >
+          <img src={img} alt='' className={"ImageLanding"}/>
         </div>
   )
 }
--- src/Parts/Home/Landing.js
+++ src/Parts/Home/Landing.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Image from '../../Compontents/Landing/Image' 
-import NavItems from '../../Compontents/Landing/NavItems/NavItems' 
+import Image from '../../Compontents/Landing/Image'
+import NavItems from '../../Compontents/Landing/NavItems/NavItems'
 import Leftdescription from '../../Compontents/Landing/Leftdescription/Leftdescription'
 import Rightdescription from '../../Compontents/Landing/Rightdescription/Rightdescription'
 import BottomQuote from "../../Compontents/Landing/BottomQuote/BottomQuote"
@@ -10,6 +10,8 @@ export default function Landing() {
         height:'100vh',
         backgroundColor:'#1f2029',
         position:'relative',
+        width:"100vw",
+        overflowX:"hidden"
       }}>
         <Image/>
         <NavItems/>
