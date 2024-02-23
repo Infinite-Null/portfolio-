289268b0d6e32b07f8e5fb06e0f99ecbd91910cc
Infinite-Null <ankit.kum.sha9933@gmail.com>
2024-02-23 21:51:17 +0530

    Made responsive about work

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Landing from '../Parts/Home/Landing'
-import AboutMe from '../Parts/Home/AboutMe'
 import {Work} from "../Parts/Home/Work";
 import {About} from "../Parts/Home/About";
 import {Skills} from "../Parts/Home/Skills";
@@ -10,7 +9,6 @@ export default function Home() {
   return (
     <>
       <Landing/>
-      {/*<AboutMe/>*/}
         <About/>
       <Work/>
       <Skills/>
